035197cc0654333bb56cc3c721ba7e7d1c167caf
fathirAmr <erchad@DESKTOP-T2PIVRS.localdomain>
2025-12-05 05:20:40 +0700

    add SPA fallback and caddy config

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/",       // ensures correct asset paths
+  build: {
+    outDir: "dist"
+  }
 })
+
